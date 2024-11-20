a99bbbcb188de9b6d181913580721b5b1eddfbb1
empedangiunhatptit <phamngoclinhtq2006@gmail.com>
2024-11-20 21:44:12 +0700

    Add files via upload

---
+++ demo01 hoc c.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main ()
+{   int a=1; // kieu so nguyen;
+    float pi=3.14; //kieu so thuc;
+    char linh="u"; // kieu ky tu;
+    double dong=16.55; //kieu so thuc co do chinh xac;
+return 0;
+}
